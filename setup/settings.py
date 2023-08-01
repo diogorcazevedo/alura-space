37cba282729f871b48568c4a441cfca7eae893b5
diogoazevedo <diogorcazevedo@gmail.com>
2023-08-01 13:15:59 -0300

    changes true

--- setup/settings.py
+++ setup/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 
 ]
 
@@ -81,11 +81,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:RHBHJqCGHpVtYytLtKJM@containers-us-west-183.railway.app:7317/railway',
+        conn_max_age=1600,
+        conn_health_checks=True,
+    )
 }
 
 
